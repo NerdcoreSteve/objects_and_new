21f5bbdbb36b6a3e137bcc71c4cfc00f3a6aed7c
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-15 00:06:36 -0600

    finished

--- index.js
+++ index.js
@@ -15,15 +15,41 @@ const
     person = (firstName, lastName) => ({
         firstName,
         lastName,
-        fullName: () => return `${firstName} ${lastName}`
-    })
+        fullName: function () {
+            return `${this.firstName} ${this.lastName}`
+        }
+    }),
+    spaceShip = function (type, engine, name) {
+        this.type = type
+        this.engine = engine
+        this.name = name
+        this.description = function () {
+            return `The ${name} is a ${type} class star ship with a ${engine} engine`
+        }
+    },
+    somePerson = person('Miss', 'Piggy')
 
-    console.log(Object.keys(spiderman))
-    console.log(superHero('Clark', 'Kent', 'Smallville'))
-    console.log(
-        superHeroWithPowers(
-            'Linda',
-            'Lee', 'Midvale',
-            ['flight', 'super strength', 'heat vision', 'ice breath', 'x-ray vision']))
+console.log(Object.keys(spiderman))
 
-    console.log(person('Kermit', 'The Frog').fullName())
+console.log(superHero('Clark', 'Kent', 'Smallville'))
+
+console.log(
+    superHeroWithPowers(
+        'Linda',
+        'Lee', 'Midvale',
+        ['flight', 'super strength', 'heat vision', 'ice breath', 'x-ray vision']))
+
+console.log(person('Kermit', 'The Frog').fullName())
+
+console.log(somePerson.fullName())
+somePerson.firstName = 'Ms'
+somePerson.lastName = 'Pacman'
+console.log(somePerson.fullName())
+
+somePerson.shoes = 'Chuck Taylor\'s'
+console.log(JSON.stringify(somePerson))
+
+console.log((new spaceShip('galaxy', 'warp', 'Enterprise')).description())
+
+spaceShip('excelsior', 'warp', 'Excelsior')
+console.log(global.description())
