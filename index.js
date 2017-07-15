f0423d910030b0f8fce7b8716d4a78760ad239cb
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-14 23:27:15 -0600

    added more examples

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('what')
+const
+    spiderman = {
+        firstName: 'Peter',
+        lastName: 'Parker',
+        homeTown: 'NewYork'
+    },
+    superHero = (firstName, lastName, homeTown) => ({
+        firstName, lastName, homeTown
+    }),
+    superHeroWithPowers = (firstName, lastName, homeTown, powers) =>
+        Object.assign(
+            {},
+            superHero(firstName, lastName, homeTown),
+            {powers}),
+    person = (firstName, lastName) => ({
+        firstName,
+        lastName,
+        fullName: () => return `${firstName} ${lastName}`
+    })
+
+    console.log(Object.keys(spiderman))
+    console.log(superHero('Clark', 'Kent', 'Smallville'))
+    console.log(
+        superHeroWithPowers(
+            'Linda',
+            'Lee', 'Midvale',
+            ['flight', 'super strength', 'heat vision', 'ice breath', 'x-ray vision']))
+
+    console.log(person('Kermit', 'The Frog').fullName())
